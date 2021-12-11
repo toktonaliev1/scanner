968fd70a0fb7473bb0dd92e21028f135fe0fb934
toktonaliev1 <toktonalievamantur66@gmail.com>
2021-12-11 22:12:17 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner Ab = new Scanner(System.in);
+        System.out.println("привет!");
+        String BC = Ab.nextLine();
+        System.out.println("привет ");
+        Scanner db = new Scanner(System.in);
+        System.out.println("как дела?");
+        String hg = db.nextLine();
+        System.out.println("хорошо нормально "  );
+
+
+
+    }
+}
